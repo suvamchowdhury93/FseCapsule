c215b98b4268440d4c1b83add12bbef56f21d555
423910 <43650175+423910@users.noreply.github.com>
2019-04-05 14:13:00 +0530

    Add files via upload

---
+++ server/SPA_WebAPI/SPA_WebAPI/Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SPA_WebAPI
+{
+    public class Task
+    {
+        public long Task_ID { get; set; }
+        public long? Parent_ID { get; set; }
+        public string Task_Name { get; set; }
+        public DateTime? Start_Date { get; set; }
+        public DateTime? End_Date { get; set; }
+        public int? Priority { get; set; }
+    }
+}
