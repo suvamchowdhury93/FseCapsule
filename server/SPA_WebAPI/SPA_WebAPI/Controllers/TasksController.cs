bd8d1bff0d18dee6d2a2c379bd51210fd877823e
423910 <43650175+423910@users.noreply.github.com>
2019-04-05 14:11:26 +0530

    Add files via upload

---
+++ server/SPA_WebAPI/SPA_WebAPI/Controllers/TasksController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Web.Http;
+using SPA_BL;
+using SPA_DL;
+
+namespace SPA_WebAPI.Controllers
+{
+    [RoutePrefix("tasks")]
+    public class TasksController : ApiController
+    {
+        TaskRepository _taskRepo = new TaskRepository();
+
+        [HttpGet, HttpPost]
+        [Route("addtask")]
+        public bool AddTask([FromBody]Task task)
+        {
+            return _taskRepo.AddTask(task);
+        }
+
+        [HttpGet, HttpPost]
+        [Route("updatetask")]
+        public bool UpdateTask([FromBody]Task task)
+        {
+            return _taskRepo.UpdateTask(task);
+        }
+
+        [HttpGet, HttpPost]
+        [Route("deletetask/{taskId}")]
+        public bool DeleteTask(long taskId)
+        {
+            return _taskRepo.DeleteTask(taskId);
+        }
+
+        [HttpGet, HttpPost]
+        [Route("viewtask/{taskId}")]
+        public Task ViewTask(long taskId)
+        {
+            return _taskRepo.ViewTask(taskId);
+        }
+
+        [HttpGet, HttpPost]
+        [Route("viewtasks")]
+        public List<TaskDetail> ViewTasks([FromBody]SearchRequest searchReq)
+        {
+            return _taskRepo.ViewTasks(searchReq);
+        }
+
+        [HttpGet, HttpPost]
+        [Route("viewalltasks")]
+        public List<TaskDetail> ViewAllTasks()
+        {
+            return _taskRepo.ViewAllTasks();
+        }
+    }
+}
