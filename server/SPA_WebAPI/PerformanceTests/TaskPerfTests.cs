40efde639b9be9613c4d5d35e545e6767b83775d
423910 <43650175+423910@users.noreply.github.com>
2019-04-05 14:16:47 +0530

    Add files via upload

---
+++ server/SPA_WebAPI/PerformanceTests/TaskPerfTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NBench;
+
+namespace PerformanceTests
+{
+    [TestClass]
+    public class TaskPerfTests
+    {
+        [PerfBenchmark(NumberOfIterations = 1, RunMode = RunMode.Throughput,
+        TestMode = TestMode.Test, SkipWarmups = true)]
+        [ElapsedTimeAssertion(MaxTimeMilliseconds = 5000)]
+        public void PerformanceTests()
+        {
+            // Set up Prerequisites   
+            var controller = new SPA_WebAPI.Controllers.TasksController();
+            // Act on Test  
+            var response = controller.ViewTask(4);
+            // Assert the result  
+            Assert.IsTrue(response != null);
+        }
+    }
+}
